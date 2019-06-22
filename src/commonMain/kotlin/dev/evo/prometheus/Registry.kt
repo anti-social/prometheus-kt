dcc27a02d6d936a28e514dd13a34067d4e9c2a6d
Alexander Koval <kovalidis@gmail.com>
2019-06-22 21:39:13 +0300

    Simplified some code

--- src/commonMain/kotlin/dev/evo/prometheus/Registry.kt
+++ src/commonMain/kotlin/dev/evo/prometheus/Registry.kt
@@ -1,7 +1,35 @@
 package dev.evo.prometheus
 
-internal expect class Registry<K, V>() {
-    val size: Int
-    suspend fun getOrPut(key: K, init: () -> V): V
-    suspend fun forEach(block: (Pair<K, V>) -> Unit)
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+internal class Registry<K, V> {
+    private val locks = Array(CONCURRENCY_LEVEL) { Mutex() }
+    private val registry = Array(CONCURRENCY_LEVEL) { HashMap<K, V>() }
+
+    companion object {
+        private const val CONCURRENCY_LEVEL = 16
+    }
+
+    private fun getIndex(key: K) = (key.hashCode() and 0x7FFF_FFFF) % CONCURRENCY_LEVEL
+
+    val size = registry.size
+
+    suspend fun getOrPut(key: K, init: () -> V): V {
+        val ix = getIndex(key)
+        val map = registry[ix]
+        return locks[ix].withLock {
+            map[key] ?: init().also { map[key] = it }
+        }
+    }
+
+    suspend fun forEach(block: (Pair<K, V>) -> Unit) {
+        (0 until CONCURRENCY_LEVEL).forEach { ix ->
+            locks[ix].withLock {
+                for (entry in registry[ix]) {
+                    block(entry.toPair())
+                }
+            }
+        }
+    }
 }
