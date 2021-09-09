36036c630505430c56deef6b885a7ed3c16222de
Alexander Koval <kovalidis@gmail.com>
2021-09-09 10:26:20 +0300

    Update gradle to 7.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,3 @@ rootProject.name = "prometheus-kt"
 project(":hotspot").name = "prometheus-kt-hotspot"
 project(":ktor").name = "prometheus-kt-ktor"
 project(":push").name = "prometheus-kt-push"
-
-enableFeaturePreview("GRADLE_METADATA")
