4a3ce8571c859a99316ebadae85f8acb9467e944
Alexander Koval <kovalidis@gmail.com>
2023-01-31 18:57:46 +0200

    Update kotlin to 1.7.22

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -20,6 +20,6 @@ idea {
 
 dependencies {
     // TODO: How could we use single kotlin version?
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
 }
